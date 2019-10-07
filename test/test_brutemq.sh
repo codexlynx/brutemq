3b9065eaa704e4be6ea2db285058ce43a09c312b
codexlynx <codexlynx@gmail.com>
2019-10-07 18:21:52 +0100

    feat(test): Add test file

--- test/test_brutemq.sh
+++ test/test_brutemq.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -d --name rabbitmq -p 5672:5672 -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=1234 rabbitmq:alpine)
+PASSWORD="supertest"
+
+CONTAINER_ID=$(docker run -d --name rabbitmq -p 5672:5672 -e RABBITMQ_DEFAULT_USER=admin -e RABBITMQ_DEFAULT_PASS=${PASSWORD} rabbitmq:alpine)
+cd ..
+
+for pass in {1..100}; do
+  uuidgen  | md5sum | cut -d'-' -f1 >> pass.txt
+done
+echo ${PASSWORD} >> pass.txt
+
+./brutemq -u admin -f pass.txt
 
 docker stop ${CONTAINER_ID}
