b8cc28e8bd8ef4a8b053473b7865aa7333471f7d
Eduardo González <codexlynx@gmail.com>
2025-04-12 18:15:54 +0200

    test(vault): Add vault test fix, new container image location

--- test/vault.sh
+++ test/vault.sh
@@ -2,7 +2,7 @@
 
 export PASSWORD="supertest"
 
-CONTAINER_ID=$(docker run -d --rm -p 8200:8200 --name vault -e VAULT_DEV_ROOT_TOKEN_ID=root vault:latest)
+CONTAINER_ID=$(docker run -d --rm -p 8200:8200 --name vault -e VAULT_DEV_ROOT_TOKEN_ID=root hashicorp/vault:latest)
 
 echo -n > passwords.txt
 for pass in {1..100}; do
@@ -12,8 +12,8 @@ echo "${PASSWORD}" >> passwords.txt
 
 sleep 20
 
-docker run -it --net=host -e VAULT_ADDR=http://0.0.0.0:8200 -e VAULT_TOKEN=root vault auth enable userpass
-docker run -it --net=host -e VAULT_ADDR=http://0.0.0.0:8200 -e VAULT_TOKEN=root vault write auth/userpass/users/root password=${PASSWORD} policies=root
+docker run -it --net=host -e VAULT_ADDR=http://0.0.0.0:8200 -e VAULT_TOKEN=root hashicorp/vault auth enable userpass
+docker run -it --net=host -e VAULT_ADDR=http://0.0.0.0:8200 -e VAULT_TOKEN=root hashicorp/vault write auth/userpass/users/root password=${PASSWORD} policies=root
 ./dist/brutemq_amd64 vault -d passwords.txt
 
 echo
