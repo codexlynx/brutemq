1dea742e1933eca24dbddd01478b5eaf21e7e7da
Eduardo González <codexlynx@gmail.com>
2025-04-12 18:07:15 +0200

    test(etcd): Add etcd test fix, latest container tag no longer available

--- test/etcd.sh
+++ test/etcd.sh
@@ -3,7 +3,7 @@
 export PASSWORD="supertest"
 
 CONTAINER_ID=$(docker run -d --rm -p 2379:2379 \
-  --name etcd quay.io/coreos/etcd:latest etcd \
+  --name etcd quay.io/coreos/etcd:v3.5.21 etcd \
   --initial-advertise-peer-urls http://0.0.0.0:2380 --listen-peer-urls http://0.0.0.0:2380 \
   --advertise-client-urls http://0.0.0.0:2379 --listen-client-urls http://0.0.0.0:2379 \
   --initial-cluster node1=http://0.0.0.0:2380 \
@@ -16,8 +16,8 @@ done
 echo "${PASSWORD}" >> passwords.txt
 
 sleep 20
-docker run -it --net=host -e ETCDCTL_API=3 quay.io/coreos/etcd etcdctl --endpoints http://0.0.0.0:2379 user add "root:${PASSWORD}"
-docker run -it --net=host -e ETCDCTL_API=3 quay.io/coreos/etcd etcdctl --endpoints http://0.0.0.0:2379 auth enable --user "root:${PASSWORD}"
+docker run -it --net=host -e ETCDCTL_API=3 quay.io/coreos/etcd:v3.5.21 etcdctl --endpoints http://0.0.0.0:2379 user add "root:${PASSWORD}"
+docker run -it --net=host -e ETCDCTL_API=3 quay.io/coreos/etcd:v3.5.21 etcdctl --endpoints http://0.0.0.0:2379 auth enable --user "root:${PASSWORD}"
 ./dist/brutemq_amd64 etcd -d passwords.txt
 
 echo
